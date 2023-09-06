42c18251e3f50b3f5f1c05292b5af9fef84fc48f
Axel Laurent <axosar@gmail.com>
2023-09-06 12:01:26 -0600

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './modules/login/login.component';
+
+const routes: Routes = [
+  { path: '', component: AppComponent }, // Define the default route
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
@@ -14,8 +22,10 @@ import { LoginComponent } from './modules/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forChild(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
